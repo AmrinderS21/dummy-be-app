74665a981af6a96f10dd537cdb43f378c674fb37
Amrinder Singh <amrinder@apyhub.com>
2023-02-02 15:30:37 +0530

    server test

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 const { initApyhub, data } = require("apyhub");
-initApyhub("APT0R8Ss23IaKkJvzYZaZKoQiXjnF1AGQnzwvr303YRaXU");
+initApyhub("APT0fbmLi30UrF6MeMGo6K339YzmvPtL7kAHgljIFvsU8gfh6sDz");
 
 const PORT = process.env.PORT || 3001;
 
@@ -29,10 +29,18 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-app.get("/currency-list", (req, res) => {
-    data.currencyList()
-    .then((resp) => res.send({ list: resp }))
-    .catch((err) => res.send({ error: err}))
+app.get("/currency-list", async (req, res) => {
+  try {
+    const resp = await data.currencyList();
+    res.send({ list: resp })
+  } catch (e) {
+    console.log(e, 'eeee');
+  } finally {
+    console.log('eeee');
+  }
+    // data.currencyList()
+    // .then((resp) => res.send({ list: resp }))
+    // .catch((err) => res.send({ err: throw new Error('ws')}))
     
 });
 
@@ -47,3 +55,5 @@ app.post("/currency-converter", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
+
+module.exports = app;
