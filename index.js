91b39338ac45e3683199b5838b5ac8fc5837ff8a
Amrinder Singh <amrinder@apyhub.com>
2023-02-02 16:14:21 +0530

    minor fixes

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const bodyParser = require('body-parser');
 const app = express();
 
 const { initApyhub, data } = require("apyhub");
+
+// please add your own token 
+// initApyhub("your-token");
 initApyhub("APT0fbmLi30UrF6MeMGo6K339YzmvPtL7kAHgljIFvsU8gfh6sDz");
 
 const PORT = process.env.PORT || 3001;
@@ -30,26 +33,15 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 app.get("/currency-list", async (req, res) => {
-  try {
-    const resp = await data.currencyList();
-    res.send({ list: resp })
-  } catch (e) {
-    console.log(e, 'eeee');
-  } finally {
-    console.log('eeee');
-  }
-    // data.currencyList()
-    // .then((resp) => res.send({ list: resp }))
-    // .catch((err) => res.send({ err: throw new Error('ws')}))
-    
+  data.currencyList()
+  .then((resp) => res.send({ list: resp }))
+  .catch((error) => res.status(400).send({ error }))
 });
 
 app.post("/currency-converter", (req, res) => {
     data.currencyConverter(req.query)
     .then((resp) => res.send({ resp }))
-    .catch((err) =>  res.status(400).send({
-        err: {...err}
-     }))
+    .catch((error) => res.status(400).send({ error }))
 });
 
 app.listen(PORT, () => {
